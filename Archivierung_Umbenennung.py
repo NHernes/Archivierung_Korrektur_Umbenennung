5f3e49ce1b4d8d87aadedf68d1ea90699c2278ee
Nils Hernes <100680594+NHernes@users.noreply.github.com>
2022-04-22 13:42:47 +0200

    Add files via upload
    Änderung bei der Benennung der Dateien --> Fach an Stelle der Lizenz

--- Archivierung_Umbenennung.py
+++ Archivierung_Umbenennung.py
@@ -40,13 +40,15 @@ for i in ordner:
             #Trennen des eingelesenen Texts in eine Liste
             text=text.split()
 
+
+            ############### Extraktion Name ###############
             #Extraktion des Indexes für Geburtsdatum und Teilnehmer, da zwischen diesen Indexen der Name der Kandidat:innen angeordnet ist
             geburtsdatum_liste = [i for i, s in enumerate(text) if 'Geburtsdatum:' in s]
             teilnehmer_liste = [i for i, s in enumerate(text) if 'Teilnehmer:' in s]
 
             #Berechnung der zwischen den beiden Randindexen befindlichen Listeneinträge, dies ist der Name der Person
             #Die Range muss daher definiert werden, damit auch längere Namen korrekt eingelesen werden
-            differenz=(teilnehmer_liste[0]-geburtsdatum_liste[0])-1
+            differenz1=(teilnehmer_liste[0]-geburtsdatum_liste[0])-1
 
             hochzählen=0
 
@@ -56,13 +58,39 @@ for i in ordner:
             name=""
 
             #Hier wird über alle Indexe zwischen den beiden Randindexen iteriert und die darin befindlichen Items dem Namen angehangen
-            while hochzählen < differenz:
+            while hochzählen < differenz1:
                 tracking_index=tracking_index+1
                 hochzählen=hochzählen+1
                 name=name+"_"+text[tracking_index]
                 #Komma wird aus dem Namen entfernt
                 name=name.replace(",","")
-            
+
+
+            ############### Extraktion Fachname ###############
+            #Extraktion des Indexes für Ergebnis und Fach, da zwischen den ersten Indexen der Fächername angeordnet ist
+            ergebnis_liste = [i for i, s in enumerate(text) if 'Ergebnis:' in s]
+            fach_liste = [i for i, s in enumerate(text) if 'Fach:' in s]
+
+
+            #Berechnung der zwischen den beiden Randindexen befindlichen Listeneinträge, dies ist der Fächername
+            #Die Range muss daher definiert werden, damit auch längere Fächernamen korrekt eingelesen werden
+            differenz2=(fach_liste[0]-ergebnis_liste[0])-1
+
+            hochzählen=0
+
+            #tracking_index wird initialisiert, um die Indexe zwischen den Randindexen zu berechnen
+            tracking_index=int(ergebnis_liste[0])
+
+            fach=""
+
+            #Hier wird über alle Indexe zwischen den beiden Randindexen iteriert und die darin befindlichen Items dem Fächernamen angehangen
+            while hochzählen < differenz2:
+                tracking_index=tracking_index+1
+                hochzählen=hochzählen+1
+                fach=fach+"_"+text[tracking_index]
+                
+
+            ############### Extraktion Matrikelnummer ###############
             #Die Matrikelnummer befindet sich im Login, dieser wird immer nach der zweiten Erscheinung des Items "Geburtsdatum:" geschrieben
             #Daher wird hier der Index aller Iterationen von "Geburtsdatum:" gebildet
             liste_matrikelnummer = [i for i, s in enumerate(text) if 'Geburtsdatum:' in s]
@@ -74,20 +102,22 @@ for i in ordner:
             sep="-"
             while sep in Matrikelnummer:
                 Matrikelnummer=Matrikelnummer[1:]
-            
-            #Hier wird das Fach aus dem Gesamtindex gefiltert und aus den beiden Items zusammengefügt
-            fach_liste = [i for i, s in enumerate(text) if 'Fach:' in s]
-            p= fach_liste[0]
-            fach=text[p+1]+" "+text[p+2]
 
+            ############### Extraktion Lizenzname ###############           
+            #Hier wird das Fach aus dem Gesamtindex gefiltert und aus den beiden Items zusammengefügt
+            lizenz_liste = [i for i, s in enumerate(text) if 'Fach:' in s]
+            p= lizenz_liste[0]
+            lizenz=text[p+1]+" "+text[p+2]
+
+            ############### Generierung der Strings zur Umbenennung ###############
             #Hier wird der String zur Umbenennung der Datei gebildet
-            string=Matrikelnummer+name+"_"+fach+".pdf"
+            string=Matrikelnummer+name+fach+".pdf"
 
             #Hier wird der Pfad zur umzubennenden Datei gebildet
             path_file_new=path1+"/"+string
 
             #Hier wird der Pfad zum umzubennenden Ordner gebildet
-            ordner_neu=path0+"/"+Matrikelnummer+name
+            ordner_neu=path0+"/"+Matrikelnummer+name+"_"+lizenz
 
         #Umbenennung der Datei und des Ordners    
         os.rename(path2, path_file_new)
